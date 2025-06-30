ef681251359eca1d1cbfbac974d47115b29ce4f7
Myles Braithwaite <github@myles.cat>
2025-06-30 14:12:25 -0400

    Fix CLI auth test for separate stderr

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -5,23 +5,35 @@ from mastodon_to_sqlite import cli
 
 
 @pytest.mark.parametrize(
-    "verify_auth_return_value, expected_stdout_startswith",
+    "verify_auth_return_value, expected_stdout, expected_stderr, expected_exit_code",
     (
-        (True, "Successfully"),
-        (False, "Failed"),
+        (True, "Successfully authenticated with the Mastodon server.\n", "", 0),
+        (
+            False,
+            "",
+            "Error: Failed to authenticate with the Mastodon server.\n",
+            1,
+        ),
     ),
 )
 def test_verify_auth(
-    verify_auth_return_value, expected_stdout_startswith, mocker
+    verify_auth_return_value,
+    expected_stdout,
+    expected_stderr,
+    expected_exit_code,
+    mocker,
 ):
     mocker.patch(
         "mastodon_to_sqlite.cli.service.verify_auth",
         return_value=verify_auth_return_value,
     )
 
-    runner = CliRunner()
+    runner = CliRunner(mix_stderr=False)
     result = runner.invoke(
-        cli.verify_auth, ["--auth", "tests/fixture-auth.json"]
+        cli.verify_auth,
+        ["--auth", "tests/fixture-auth.json"],
     )
 
-    assert result.stdout.startswith(expected_stdout_startswith)
+    assert result.stdout == expected_stdout
+    assert result.stderr == expected_stderr
+    assert result.exit_code == expected_exit_code
